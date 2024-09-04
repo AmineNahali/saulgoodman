b83a6d76d6949beb6d860ca5228fa25aec9b5225
Amine <nahali.amine.32@gmail.com>
2024-09-04 15:21:09 +0200

    finished kernel mode driver

--- km/src/main.cpp
+++ km/src/main.cpp
@@ -7,9 +7,17 @@ extern "C" {
 	NTKERNELAPI NTSTATUS MmCopyVirtualMemory(PEPROCESS SourceProcess, PVOID SourceAdress,
 		                                     PEPROCESS TargetProcess, PVOID TargetAdress,
 		                                     SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode,
-		                                     SIZE_T ReturnSize);
+		                                     PSIZE_T ReturnSize);
 }
 
+void debug_print(PCSTR text) {
+
+#ifndef DEBUG
+	UNREFERENCED_PARAMETER(text);
+#endif // !DEBUG
+
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, text));
+}
 
 namespace driver {
 	namespace codes {
@@ -44,20 +52,65 @@ namespace driver {
 	}
 	//Note:Todo
 	NTSTATUS device_control(PDEVICE_OBJECT device_object, PIRP irp) {
-		UNREFERENCED_PARAMETER(device_object);     //
-		IoCompleteRequest(irp, IO_NO_INCREMENT);   //
-		return irp->IoStatus.Status;               //
+		UNREFERENCED_PARAMETER(device_object);
+
+		debug_print("[+] Device control is called.\n");
+
+		NTSTATUS status = STATUS_UNSUCCESSFUL;
+		PIO_STACK_LOCATION stack_irp = IoGetCurrentIrpStackLocation(irp);
+
+		auto request = reinterpret_cast<Request*>(irp->AssociatedIrp.SystemBuffer);
+
+		if (stack_irp == nullptr || request == nullptr) {
+			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			return status;
+		}
+
+		static PEPROCESS target_process = nullptr;
+
+		const ULONG control_code = stack_irp->Parameters.DeviceIoControl.IoControlCode;
+
+		switch (control_code)
+		{
+		case codes::attach:
+			status = PsLookupProcessByProcessId(request->process_id, &target_process);
+			break;
+		case codes::read:
+			if (target_process != nullptr) {
+				status = MmCopyVirtualMemory(
+					target_process,
+					request->target,
+					PsGetCurrentProcess(),
+					request->buffer,
+					request->size,
+					KernelMode,
+					&request->return_size
+				);
+			}
+			break;
+		case codes::write:
+			if (target_process != nullptr) {
+				status = MmCopyVirtualMemory(
+					PsGetCurrentProcess(),
+					request->buffer,
+					target_process,
+					request->target,
+					request->size,
+					KernelMode,
+					&request->return_size
+			);
+		}
+			break;
+		default:
+			break;
+		}
+
+		irp->IoStatus.Status = status;
+		irp->IoStatus.Information = sizeof(Request);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+		return status;
 	}
-}
-
-
-void debug_print(PCSTR text) {
-
-#ifndef DEBUG
-	UNREFERENCED_PARAMETER(text);
-#endif // !DEBUG
-
-	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, text));
 }
 
 
@@ -74,7 +127,8 @@ NTSTATUS DriverMain(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path)
 		                             &device_name,
 		                             FILE_DEVICE_UNKNOWN,
 		                             FILE_DEVICE_SECURE_OPEN,
-		                             FALSE, &device_object);
+		                             FALSE,
+                                     &device_object);
 	if (status != STATUS_SUCCESS) {
 		debug_print("[-] Failed to create driver device.\n");
 	}
@@ -93,9 +147,17 @@ NTSTATUS DriverMain(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path)
 
 	//setup communication between um & km
 	SetFlag(device_object->Flags, DO_BUFFERED_IO);
-	driver_object->MajorFunction[IRP_MJ_CREATE] = driver::create;
 
-	return STATUS_SUCCESS;
+	//setup driver handlers
+	driver_object->MajorFunction[IRP_MJ_CREATE] = driver::create;
+	driver_object->MajorFunction[IRP_MJ_CLOSE] = driver::close;
+	driver_object->MajorFunction[IRP_MJ_DEVICE_CONTROL] = driver::device_control;
+
+	ClearFlag(device_object->Flags, DO_DEVICE_INITIALIZING);
+
+	debug_print("[+] Driver initialized successfully.\n");
+
+	return status;
 }
 
 
